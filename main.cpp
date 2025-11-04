ee2ecc771913496d2a7a1b6619f2933dc488bec0
ElisaHann <1116elisahan@gmail.com>
2025-11-03 18:05:15 -0800

    commit/push

--- main.cpp
+++ main.cpp
@@ -89,30 +89,44 @@ int createLeafNodes(int freq[]) {
 
 // Step 3: Build the encoding tree using heap operations
 int buildEncodingTree(int nextFree) {
+    //checks if there are any leaf nodes
+    if (nextFree == 0) {
+        return -1;
+    }
+    if (nextFree == 1) {
+        return 0;
+    }
+
     MinHeap heap;
+
     //pushes leaves into heap
     for (int i = 0; i < nextFree; ++i) {
         heap.push(i, weightArr);
     }
     //makes the next free for the internal nodes
     int curNext = nextFree;
+
     while (heap.size > 1) {
         int leftIdx = heap.pop(weightArr);
         int rightIdx = heap.pop(weightArr);
+
     //displays an error if the next node exceeds the max amount of nodes
         if (curNext >= MAX_NODES) {
             cerr << "Error: too many nodes.\n";
             exit(1);
         }
+
        //creates the parent and internal node
         int parent = curNext++;
         weightArr[parent] = weightArr[leftIdx] + weightArr[rightIdx];
         leftArr[parent] = leftIdx;
         rightArr[parent] = rightIdx;
         charArr[parent] = '\0';
+
         //pushes the node to the heap
         heap.push(parent, weightArr);
     }
+
     //the node remaining is the root node
     int root = heap.pop(weightArr);
     return root;
@@ -125,31 +139,34 @@ void generateCodes(int root, string codes[]) {
     stack.push({root, ""});
 
     while (!stack.empty()) {
+        //compiler stores cur as the top node
         auto cur = stack.top();
         stack.pop();
         int node = cur.first;
-        const string &path = cur.second;
+        const string& path = cur.second;
 
-        bool isLeaf = (leftArr[node] == -1 || rightArr[node] == -1);
+        //checks if the node is a leaf
+        bool isLeaf = (leftArr[node] == -1 && rightArr[node] == -1);
+
+        //if the node is a leaf, the character is recieved and saved as the code
         if (isLeaf) {
             char ch = charArr[node];
             if (ch >= 'a' && ch <= 'z') {
-                //assigns 0 if there is only one unique character
+                //assigns 0 if there is only one character
                 codes[ch - 'a'] = path.empty() ? string("0") : path;
             }
+        }
+        //pushes children in heap
             else {
                 if (rightArr[node] != -1) {
                     stack.push({rightArr[node], path + "1"});
+                }
                     if (leftArr[node] != -1) {
                         stack.push({leftArr[node], path + "0"});
                     }
                 }
             }
         }
-    }
-    // Left edge adds '0', right edge adds '1'..
-    // Record code when a leaf node is reached.
-}
 
 // Step 5: Print table and encoded message
 void encodeMessage(const string& filename, string codes[]) {
